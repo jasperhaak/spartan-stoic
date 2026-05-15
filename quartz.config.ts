bc894ee1925fcbd491952109a4ee4f4605d381b6
jasperhaak <118828984+jasperhaak@users.noreply.github.com>
2026-05-15 14:41:46 +0200

    vercel

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "spartan-stoic.vercel.app",
+    baseUrl: "spartan-stoic.vercel.app/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
